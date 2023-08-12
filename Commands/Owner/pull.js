70cc0047c0fc58b2c79a464ea0638f32554d8b8d
Arijit Debnath <itsarijitdebnath@gmail.com>
2023-08-13 01:14:39 +0530

    Done

---
+++ Commands/Owner/pull.js
@@ -0,0 +1,31 @@
+const { Message, Client } = require("discord.js");
+const { exec } = require("child_process");
+
+module.exports = {
+  name: "pull",
+  description: "Pull code to root",
+  aliases: [],
+  args: false,
+  usage: "",
+  permissions: "",
+  botPerms: "",
+  ownerOnly: true,
+  cooldown: 0,
+  category: "",
+  premium: false,
+  voteOnly: false,
+  /**
+   * @param {Message} message
+   * @param {Array} args
+   * @param {Client} client
+   */
+  async execute(message, args, client) {
+    exec("npm run pull", (error, stdout, stderr) => {
+      if (error) return message.reply(`${error.message}`);
+
+      message.channel.send(
+        "Pulled. Please wait for 5 seconds more before reloading."
+      );
+    });
+  },
+};
