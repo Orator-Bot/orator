9e4b04476501f98406b7df1c1011eecb45b6f8a8
Arijit Debnath <itsarijitdebnath@gmail.com>
2023-08-13 00:42:09 +0530

    Autoposter

--- Events/Client/Ready.js
+++ Events/Client/Ready.js
@@ -2,11 +2,14 @@ const { loadLegacy } = require("#handlers/legacyHandler.js");
 const { loadCommands } = require("#handlers/commandHandler.js");
 const { EmbedBuilder, ActivityType } = require("discord.js");
 const color = require("colors");
+const { AutoPoster } = require("topgg-autoposter");
 const ms = require("ms");
+
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
+    const poster = AutoPoster(client.config.TOPGGTOKEN, client);
     client.logger("├─ Client is ready to use.", "success");
     client.logger("├─ Online in " + client.guilds.cache.size + " guilds");
     const prefix = client.config.Prefix;
@@ -20,5 +23,11 @@ module.exports = {
     });
     loadCommands(client);
     loadLegacy(client);
+    poster.on("posted", (stats) => {
+      client.logger(
+        `Posted stats to Top.gg | ${stats.serverCount} servers`,
+        "success"
+      );
+    });
   },
 };
