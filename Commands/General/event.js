66738990427262e3fb9fdaf92ee5ddfbf47af997
Arijit Debnath <itsarijitdebnath@gmail.com>
2023-08-15 12:04:14 +0530

    Update event.js

--- Commands/General/event.js
+++ Commands/General/event.js
@@ -10,6 +10,7 @@ module.exports = {
   botPerms: "",
   ownerOnly: false,
   cooldown: 0,
+  stop: true,
   category: "general",
   premium: false,
   voteOnly: false,
@@ -20,8 +21,29 @@ module.exports = {
    */
   async execute(message, args, client) {
     const embed = new EmbedBuilder()
-      .setDescription("Comming Soon...")
-      .setColor(client.color);
+      .setAuthor({
+        name: message.author.username,
+        iconURL: message.author.displayAvatarURL(),
+      })
+      .setTitle("Orator Voting Event")
+      .setDescription(
+        "Introducing **Voting Event**:\nIn this event you'll vote Orator every 12 hours. The top 5 winners will get rewards"
+      )
+      .addFields(
+        {
+          name: "Rewards:",
+          value:
+            "1st. One Year Premium - Free\n2nd. 3 Months Premium - Free\n3rd. 1 Month Premium - Free\n4th. 50% Discount on Lifetime and Yearly Plans\n5th. 25% Discount on Lifetime and Yearly plans.",
+        },
+        {
+          name: "Eligibility:",
+          value:
+            "1. You must take part in the event only from a single account.\n2. You must collect atleast 10 votes.",
+        }
+      )
+      .setColor(client.color)
+      .setTimestamp()
+      .setThumbnail(client.user.displayAvatarURL());
 
     message.channel.send({
       embeds: [embed],
