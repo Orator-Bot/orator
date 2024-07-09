02dc1086c74c4f5ca1b1e6bb366894b7b5c2265d
Arijit Debnath <75007470+arijitdn@users.noreply.github.com>
2024-07-09 17:28:06 +0530

    Update Legacy.js

--- Events/Message/Legacy.js
+++ Events/Message/Legacy.js
@@ -80,14 +80,6 @@ module.exports = {
         "warn"
       );
     if (command) {
-      if (!client.config.owners.includes(message.author.id)) {
-        const banData = client.getbanneduser.get(message.author.id);
-        if (banData) {
-          return message.reply(
-            "Sorry we can't process your request!\n**Reason:** You're blacklisted from using the bot. If you think its a mistake, then create a ticket in our support server."
-          );
-        }
-      }
       const incrementCommandCount = (cmdName) => {
         client.statsdb
           .prepare(
